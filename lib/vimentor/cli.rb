b36c9ff1c07364b2fbd43fd5dc0ae6df08a65264
Genki <cfhoyuk.reccos.nelg@gmail.com>
2012-12-02 16:02:21 +0900

    Add code to create hash from keylog

--- lib/vimentor/cli.rb
+++ lib/vimentor/cli.rb
@@ -1,12 +1,22 @@
 module Vimentor
 
+  LOGFILE = "/tmp/vimentor_keylog"
+
   class CLI < Thor
     desc "vim ARGS", "launch Vim session"
     def vim(*args)
-      vimcmd = "vim -W ~/vimkeylog #{args.join(" ")}"
+      vimcmd = "vim -W #{LOGFILE} #{args.join(" ")}"
       say "Invoking: #{vimcmd}"
       system(vimcmd)
       say "End vim session."
+
+      # Parse keylog
+      content = File.read(LOGFILE)
+      count = Hash.new(0)
+      content.split(//).each { |c|
+        count[c] += 1
+      }
+      say count
     end
   end
 
