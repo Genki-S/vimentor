2822908d5def5053e3ecce2edd45fd45133e1681
Genki <cfhoyuk.reccos.nelg@gmail.com>
2012-12-04 12:04:06 +0900

    Use unique temp file for log data (to allow multiple sessions)

--- lib/vimentor/cli.rb
+++ lib/vimentor/cli.rb
@@ -1,14 +1,14 @@
 module Vimentor
 
-  LOGFILE = "/tmp/vimentor_keylog"
-  SAVEROOT = Dir.home + "/vimentor";
+  SAVEROOT = Dir.home + "/vimentor"
 
   class CLI < Thor
     desc "vim ARGS", "launch Vim session"
     def vim(*args)
       info = MetaInfo.new()
+      logfile = Tempfile.new("vimkeylog")
 
-      vimcmd = "vim -W #{LOGFILE} #{args.join(" ")}"
+      vimcmd = "vim -W #{logfile.path} #{args.join(" ")}"
       say "Invoking: #{vimcmd}"
       system(vimcmd)
       say "End vim session."
@@ -16,7 +16,7 @@ module Vimentor
       info.set_end_time()
 
       # Parse keylog
-      log = Keylog.new(File.read(LOGFILE))
+      log = Keylog.new(logfile.read)
       say log.to_a.frequency
 
       # Create directory
@@ -28,7 +28,8 @@ module Vimentor
       fn_r = d + "/" + unix_t
 
       # Save keylog
-      FileUtils.mv(LOGFILE, fn_r + ".keylog")
+      FileUtils.cp(logfile.path, fn_r + ".keylog")
+      logfile.close
 
       # Save meta info
       info.store(fn_r + ".meta")
