04b273e5c4c427b503766e11bf3645bdff92e429
Genki <cfhoyuk.reccos.nelg@gmail.com>
2012-12-17 13:22:38 +0900

    Create rdo method (for syntactic suger)

--- lib/vimentor/rclient.rb
+++ lib/vimentor/rclient.rb
@@ -1,11 +1,18 @@
 module Vimentor
   class Rclient
+
     def initialize()
       @connection = Rserve::Connection.new
     end
 
     def version()
-      @connection.eval("R.version.string").as_string
+      rdo("R.version.string").as_string
     end
+
+    private
+    def rdo(str)
+      @connection.eval(str)
+    end
+
   end
 end
